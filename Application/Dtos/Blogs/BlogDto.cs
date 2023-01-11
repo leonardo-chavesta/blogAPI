b665d7769664c9a61bb4e56840cd07a5f15fdb25
leonardo-chavesta <chavestatorresleonardo@gmail.com>
2023-01-11 13:43:51 -0500

    resolviendo conlfictos

---
+++ Application/Dtos/Blogs/BlogDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos.Blogs
+{
+    public class BlogDto
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string Comentario { get; set; }
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+        public int Estado { get; set; } = 1;
+    }
+}
